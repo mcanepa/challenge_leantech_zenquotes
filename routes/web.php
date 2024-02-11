063d542d3b17c0ccc5d1a5ef4e2e7dfdd12d0278
Matías Cánepa <matias.canepa@gmail.com>
2024-02-11 17:03:54 -0300

    Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/", [QuoteController::class, "today"])->name("quote.index");
-Route::get("today", [QuoteController::class, "today"])->name("quote.today");
-Route::get("today/new", [QuoteController::class, "new"])->name("quote.new");
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
